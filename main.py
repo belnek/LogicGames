d820645045e2ac7d122894be3dbbc52cd409e32f
Белеко Никита <belnek.it.os@gmail.com>
2023-11-07 19:18:28 +0400

    Добавлен интеллект, улучшена генерация, добавлена система определения победы(не тестировалась), поправлены мелкие баги со стрельбой и танками

--- main.py
+++ main.py
@@ -32,12 +32,13 @@ class Main(QMainWindow):
         self.playerTanks = list()
         self.AITanks = list()
         self.initUI()
+        self.isPlayerTurn = True
 
     def initUI(self):
         uipath = os.path.join(os.path.dirname(__file__), "ui.ui")
         uic.loadUi(uipath, self)
 
-        self.setFixedSize(727, 886)
+        # self.setFixedSize(727, 886)
         self.playerTanks = [Tank(self) for i in range(30)]
         self.AITanks = [Tank(self) for i in range(30)]
         c = 0
@@ -90,9 +91,18 @@ class Main(QMainWindow):
             random.seed(random.randint(0, 10000000))
 
             for j in range(segmentsy):
-                i += random.randint(0, 2)
-                j += random.randint(0, 1)
+                ii = random.randint(0, 2)
 
+                jj = random.randint(0, 1)
+                while i + ii > segmentsx:
+                    ii = random.randint(0, 2)
+
+                i += ii
+
+                while j + jj > segmentsx:
+                    jj = random.randint(0, 1)
+
+                j += jj
                 listOfPotencialSegments.append((i, j))
         n = len(self.playerTanks)
         random.seed(random.randint(0, 10000000))
@@ -117,7 +127,7 @@ class Main(QMainWindow):
 
         width = self.AITanks[0].width()
         height = self.AITanks[0].height()
-        segmentsx = int(660 / width)
+        segmentsx = int(500 / width)
         segmentsy = int((360) / height)
         listOfPotencialSegments = list()
         print("s")
@@ -125,9 +135,18 @@ class Main(QMainWindow):
             random.seed(random.randint(0, 10000000))
 
             for j in range(segmentsy):
-                i += random.randint(0, 2)
-                j += random.randint(0, 1)
+                ii = random.randint(0, 2)
 
+                jj = random.randint(0, 1)
+                while i + ii > segmentsx:
+                    ii = random.randint(0, 2)
+
+                i += ii
+
+                while j + jj > segmentsx:
+                    jj = random.randint(0, 1)
+
+                j += jj
                 listOfPotencialSegments.append((i, j))
         n = len(self.AITanks)
         random.seed(random.randint(0, 10000000))
@@ -158,12 +177,20 @@ class Main(QMainWindow):
         self.verticalScroll.setValue(int(self.currentTank.angleY))
         self.lcdVertical.display(self.currentTank.angleY)
 
-    def boom(self, bullet: QWidget):
-        ul = bullet.geometry().topLeft()
-        br = bullet.geometry().bottomRight()
-        isTank = False
+    def checkHit(self, pos):
         shootedTank = Tank(self)
-
+        ul = pos.geometry().topLeft()
+        br = pos.geometry().bottomRight()
+        isTank = False
+        for tank in self.AITanks:
+            ult = tank.geometry().topLeft()
+            brt = tank.geometry().bottomRight()
+            if ult.x() <= ul.x() <= brt.x() and ult.y() <= ul.y() <= brt.y() and ult.x() <= br.x() <= brt.x() and ult.y() <= br.y() <= brt.y():
+                isTank = True
+                shootedTank = tank
+        if not self.isPlayerTurn:
+            isTank = False
+            shootedTank = Tank()
         for tank in self.playerTanks:
             ult = tank.geometry().topLeft()
             brt = tank.geometry().bottomRight()
@@ -171,17 +198,19 @@ class Main(QMainWindow):
                 isTank = True
                 shootedTank = tank
 
-        for tank in self.AITanks:
-            ult = tank.geometry().topLeft()
-            brt = tank.geometry().bottomRight()
-            if ult.x() <= ul.x() <= brt.x() and ult.y() <= ul.y() <= brt.y() and ult.x() <= br.x() <= brt.x() and ult.y() <= br.y() <= brt.y():
-                isTank = True
-                shootedTank = tank
+        return isTank, shootedTank
 
+    def boom(self, bullet: QWidget):
+        isTank, shootedTank = self.checkHit(bullet)
         if isTank:
 
             shootedTank.shooted()
+            if shootedTank.player:
+                self.playerTanks.remove(shootedTank)
+            else:
+                self.AITanks.remove(shootedTank)
             self.layout().removeWidget(bullet)
+            print(len(self.playerTanks), len(self.AITanks))
             bullet.deleteLater()
 
             bullet.destroy(True)
@@ -197,49 +226,81 @@ class Main(QMainWindow):
         for tank in self.playerTanks:
             tank.isShooting = False
         self.currentTank.selected = False
+        if self.currentTank == shootedTank:
+            self.currentTank.shooted()
+
         self.currentTank = Tank()
         self.widget.show()
         self.horizontalScroll.setValue(0)
         self.lcdHorizontal.display(0)
         self.verticalScroll.setValue(0)
         self.lcdVertical.display(0)
+        shootings = 0
+        playerTanks = 0
+        if self.isPlayerTurn:
+            self.widget.hide()
+            for tank in self.playerTanks:
+                shootings += tank.shootsEstimated
+            if shootings == 0 and len(self.AITanks) > 0:
 
-    def tick(self, start):
-        if self.currentTank.angleX != self.horizontalScroll.value():
-            if self.horizontalScroll.value() > 0:
+                if len(self.playerTanks) > 0:
+                    print(f"Игра окончена! У вас осталось {len(self.playerTanks)} танков и не осталось снярядов. У соперника осталось {len(self.AITanks)}")
+                else:
+                    print(f"Игра окончена! У вас не осталось танков. У соперника осталось {len(self.AITanks)}")
+            if len(self.AITanks) == 0:
+                print(f"Вы победили! Вы уничтожили все танки соперника. У вас осталось {len(self.playerTanks)}")
+            shootings = 0
+            for tank in self.AITanks:
+                shootings += tank.shootsEstimated
+            if shootings == 0:
+                print(f"Вы победили! Соперник сдался, так как у него закончились снаряды. У вас осталось {len(self.playerTanks)}")
+            self.isPlayerTurn = False
+
+            self.AITurn()
+        else:
+            for tank in self.AITanks:
+                shootings += tank.shootsEstimated
+
+            self.isPlayerTurn = True
+
+    def tick(self, start, angleX):
+
+        if self.currentTank.angleX != angleX:
+            if angleX > self.currentTank.angleX:
                 self.currentTank.rotate(1)
                 self.currentTank.angleX += 1
-                QTimer.singleShot(50, lambda: self.tick(start))
-            elif self.horizontalScroll.value() < 0:
+                QTimer.singleShot(50, lambda: self.tick(start, angleX))
+            elif angleX < self.currentTank.angleX:
                 self.currentTank.rotate(-1)
                 self.currentTank.angleX -= 1
-                QTimer.singleShot(50, lambda: self.tick(start))
+                QTimer.singleShot(50, lambda: self.tick(start, angleX))
             else:
                 if self.currentTank.angleX > 0:
                     self.currentTank.rotate(-1)
                     self.currentTank.angleX -= 1
-                    QTimer.singleShot(50, lambda: self.tick(start))
+                    QTimer.singleShot(50, lambda: self.tick(start, angleX))
                 else:
                     self.currentTank.rotate(1)
                     self.currentTank.angleX += 1
-                    QTimer.singleShot(50, lambda: self.tick(start))
+                    QTimer.singleShot(50, lambda: self.tick(start, angleX))
+            print(self.currentTank.angleX, angleX)
 
         else:
             self.currentTank.shootsEstimated -= 1
             self.bullet = QWidget(self)
             self.bullet.setStyleSheet("background-color:black;border-radius:5px;")
             self.bullet.resize(10, 10)
-            self.bullet.move(int(self.currentTank.x + self.currentTank.width() / 3),
-                             int(self.currentTank.y + self.currentTank.height() / 2))
+
+            self.bullet.move(int(start.x + start.width() / 3),
+                             int(start.y + start.height() / 2))
             self.bullet.show()
 
-            tx, ty, t, h = self.solveTargetXY(start, self.shootSpeed, self.verticalScroll.value(),
-                                              self.horizontalScroll.value())
-
+            tx, ty, t, h = self.solveTargetXY(start, self.shootSpeed, self.currentTank.angleY,
+                                              self.currentTank.angleX)
             t = int(t * 1000 / 3)
             self.anim = QPropertyAnimation(self.bullet, b"pos")
             self.anim.setEndValue(
-                QPoint(int(tx + self.currentTank.width() / 2), int(ty + self.currentTank.height() / 3)))
+                QPoint(int(tx + start.width() / 3), int(ty + start.height() / 2)))
             self.anim.setDuration(t)
             self.anim.finished.connect(lambda: self.boom(self.bullet))
             self.anim2 = QPropertyAnimation(self.bullet, b"size")
@@ -256,19 +317,95 @@ class Main(QMainWindow):
             self.anim_group.addAnimation(self.anim_group2)
             self.anim_group.start()
 
+    def AITurn(self):
+        currentAITank = random.choice(self.AITanks)
+        sh = currentAITank.shootsEstimated
+        while sh == 0:
+            currentAITank = random.choice(self.AITanks)
+            sh = currentAITank.shootsEstimated
+        isHitNum = random.randint(0, 100)
+        print(isHitNum)
+        if isHitNum >= 40:
+            recurs = 0
+            angleX = random.randint(-90, 90)
+            angleY = random.randint(0, 90)
+            self.currentTank = currentAITank
+            self.currentTank.angleY = angleY
+            bull = QWidget()
+            bull.resize(10, 10)
+
+            bull.move(int(self.currentTank.x + self.currentTank.width() / 3),
+                      int(self.currentTank.y + self.currentTank.height() / 2))
+            x, y, _, __ = self.solveTargetXY(bull, self.shootSpeed, angleY, angleX)
+            bull.move(int(x), int(y))
+            res, _ = self.checkHit(bull)
+            print(res)
+            while not res:
+                recurs += 1
+                angleX = 180 + random.randint(-90, 90)
+                angleY = random.randint(0, 90)
+                self.currentTank = currentAITank
+                self.currentTank.angleY = angleY
+                bull = QWidget()
+                bull.resize(10, 10)
+
+                bull.move(int(self.currentTank.x + self.currentTank.width() / 3),
+                          int(self.currentTank.y + self.currentTank.height() / 2))
+                x, y, _, __ = self.solveTargetXY(bull, self.shootSpeed, angleY, angleX)
+                bull.move(int(x), int(y))
+                res, _ = self.checkHit(bull)
+                if recurs > 2000:
+                    self.AITurn()
+                    return
+                print(res)
+
+            QTimer.singleShot(50, lambda: self.tick(self.currentTank, angleX))
+
+        else:
+            angleX = 180 + random.randint(-90, 90)
+            angleY = random.randint(0, 90)
+            self.currentTank = currentAITank
+            self.currentTank.angleY = angleY
+            bull = QWidget()
+            bull.resize(10, 10)
+            bull.move(int(self.currentTank.x + self.currentTank.width() / 3),
+                      int(self.currentTank.y + self.currentTank.height() / 2))
+            x, y, _, __ = self.solveTargetXY(bull, self.shootSpeed, angleY, angleX)
+            bull.move(int(x), int(y))
+            res, _ = self.checkHit(bull)
+            while res:
+                angleX = random.randint(-90, 90)
+                angleY = random.randint(0, 90)
+                self.currentTank = currentAITank
+                self.currentTank.angleY = angleY
+                bull = QWidget()
+                bull.resize(10, 10)
+                bull.move(int(self.currentTank.x + self.currentTank.width() / 3),
+                          int(self.currentTank.y + self.currentTank.height() / 2))
+                x, y, _, __ = self.solveTargetXY(bull, self.shootSpeed, angleY, angleX)
+                bull.move(int(x), int(y))
+                res, _ = self.checkHit(bull)
+                print(res)
+
+            QTimer.singleShot(50, lambda: self.tick(self.currentTank, angleX))
+
     def makeShoot(self, start):
 
         if self.currentTank.isInit:
-            for t in self.playerTanks:
-                t.isShooting = True
+
             if self.currentTank.shootsEstimated > 0 and self.verticalScroll.value() != 0:
+                for t in self.playerTanks:
+                    t.isShooting = True
                 self.currentTank.angleY = self.verticalScroll.value()
                 self.widget.hide()
-                QTimer.singleShot(50, lambda: self.tick(start))
+                QTimer.singleShot(50, lambda: self.tick(start, self.horizontalScroll.value()))
 
     def solveTargetXY(self, start, speed, angleY, angleX):
         sx = start.x
         sy = start.y
+        if type(sx) != float and type(sx) != int:
+            sx = start.x()
+            sy = start.y()
         t = (2 * speed * sin(angleY * pi / 180)) / self.g
         r = speed * cos(angleY * pi / 180) * t
         maxx = r * cos((angleX - 90) * pi / 180)
@@ -284,13 +421,6 @@ class Main(QMainWindow):
 def excepthook(self, exc_type, exc_value, exc_tb):
     tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
     print("Oбнаружена ошибка !:", tb)
-
-
-class Bullett(QWidget):
-
-    def init(self, x, y):
-        self.x = x
-        self.y = y
 
 
 if __name__ == '__main__':
